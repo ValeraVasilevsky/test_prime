f201ecf496886dc2d92583a41e621b0ac1182482
ValeraVasilevsky <valera200208@gmail.com>
2024-08-14 22:06:01 +0700

    feat: config, tailwind & scss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  content: ["./src/**/*.{ts,tsx,vue}"],
+};
