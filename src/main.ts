c52db60507c4ef95fd0e0527425b048f149aaaff
ValeraVasilevsky <valera200208@gmail.com>
2024-08-14 22:08:16 +0700

    feat: install prime vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "./style.css";
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.mount("#app");
