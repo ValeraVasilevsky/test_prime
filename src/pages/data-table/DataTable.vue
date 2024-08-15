0ddc802404bb94545272196da366ee2ba7d8906f
ValeraVasilevsky <valera200208@gmail.com>
2024-08-15 17:44:36 +0700

    feat: added router

---
+++ src/pages/data-table/DataTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <VDataTable :value="dataTableValue" :header="tableHeader" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { VDataTable, type VDataTableHeader } from "shared/ui";
+
+import { prepareApiString } from "shared/utils/string";
+
+const tableHeader = ref<VDataTableHeader[]>([
+  {
+    field: "time",
+    header: "Время",
+  },
+  {
+    field: "value",
+    header: "Значение",
+  },
+]);
+
+const dataTableValue = prepareApiString();
+</script>
---
+++ src/pages/select/Select.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <VSelect v-model="selectedCountries" :options="COUNTRIES" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { VSelect } from "shared/ui";
+
+import { COUNTRIES } from "shared/constants";
+
+const selectedCountries = ref<{ name: string; code: string }[]>([]);
+</script>
